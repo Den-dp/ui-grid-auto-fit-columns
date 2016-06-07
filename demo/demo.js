2e925e0fcddd36de1a9852471a210e9e262f379b
Denis Bendrikov <Denis.Bendrikov@gmail.com>
2016-06-07 20:31:20 +0300

    docs(example): better demo code
    use 'name' instead of legacy 'field'
    
    add simple sortByLength

--- demo/demo.js
+++ demo/demo.js
@@ -7,35 +7,41 @@ angular.module('autoFitColumnsExample', [
 
 MainController.$inject = ['$http'];
 function MainController($http){
-
+    function sortByLength(a, b) {
+        return (a || '').length - (b || '').length;
+    }
     this.gridOptions = {
         paginationPageSizes: [25, 50, 100, 200, 500],
         paginationPageSize: 500,
+        enableFiltering: true,
         enableColumnMenus: false,
         columnDefs: [
-            { field: 'id', type: 'number' },
-            { field: 'name' },
-            { field: 'address.city', width: 100 }, //respect width
-            { field: 'address.state' },
-            { field: 'age' },
-            { displayName: 'age again', field: 'age' }, //works ok with aliases
-            { field: 'guid' },
-            { field: 'registered', cellFilter: 'date:"yyyy-MM-dd"' }, //can handle filters
-            { field: 'registered', displayName: 'registered2', cellFilter: 'date:"medium"' },
-            { field: 'picture' },
-            { field: 'address.street' },
-            { field: 'address.city' },
-            { field: 'address.state' },
-            { field: 'address.zip' },
-            { field: 'company' },
-            { field: 'email' },
-            { field: 'phone' },
-            { field: 'about' },
-            { field: 'friends[0].name', displayName: '1st friend' }, //works fine with "flat entities"
-            { field: 'friends[1].name', displayName: '2nd friend' },
-            { field: 'friends[2].name', displayName: '3rd friend' }
+            { name: 'id', type: 'number' },
+            { name: 'name' },
+            { name: 'address.city', width: 100 }, //respect width
+            { name: 'address.state' },
+            { name: 'address.zip' },
+            { name: 'age' },
+            { name: 'age again', field: 'age', displayName: 'age again' }, //works ok with aliases
+            { field: 'age', displayName: 'third age in a row' }, //works ok with aliases
+            { name: 'guid' },
+            { name: 'registered', cellFilter: 'date:"yyyy-MM-dd"' }, //can handle filters
+            { name: 'registered', displayName: 'registered2', cellFilter: 'date:"medium"' },
+            { name: 'picture' },
+            { name: 'company' },
+            { name: 'email' },
+            { name: 'phone' },
+            { name: 'about' },
+            { name: 'friends[0].name', displayName: '1st friend' }, //works fine with "flat entities"
+            { name: 'friends[1].name', displayName: '2nd friend' },
+            { name: 'friends[2].name', displayName: '3rd friend' }
         ]
     };
+
+    this.gridOptions.columnDefs = this.gridOptions.columnDefs.map(function (colDef) {
+        colDef.sortingAlgorithm = sortByLength;
+        return colDef;
+    });
 
     $http.get('500_complex.json')
         .then(function(response) {
