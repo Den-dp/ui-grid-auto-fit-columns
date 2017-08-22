81d25653e89bf3a3c870bc2907951bb0e08c022e
Gioele Masini <gioele.masini@maggioli.it>
2017-08-22 17:38:26 +0200

    feat(headerCellFilter): add support to 'headerCellFilter' option

--- src/UiGridAutoFitColumnsService.ts
+++ src/UiGridAutoFitColumnsService.ts
@@ -102,7 +102,13 @@ export class UiGridAutoFitColumnsService {
 
             if (column.colDef.enableColumnAutoFit) {
                 const columnKey = column.field || column.name;
-                optimalWidths[columnKey] = Measurer.measureRoundedTextWidth(column.displayName, this.gridMetrics.getHeaderFont()) + this.gridMetrics.getHeaderButtonsWidth();
+
+                let headerText = column.displayName;
+
+                if (!!column.colDef.headerCellFilter) {
+                    headerText = this.getFilteredValue(headerText, column.colDef.headerCellFilter);
+                }
+                optimalWidths[columnKey] = Measurer.measureRoundedTextWidth(headerText, this.gridMetrics.getHeaderFont()) + this.gridMetrics.getHeaderButtonsWidth();
 
                 rows.forEach((row) => {
                     let cellText = get(row.entity, columnKey);
